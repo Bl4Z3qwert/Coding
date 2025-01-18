2b030ee590b19abc1cf0c3179a6ae5a7078df307
Bl4Z3qwert <boubounehtoboh@gmail.com>
2025-01-18 19:10:58 +0000

    oop git

---
+++ oop.py
@@ -0,0 +1,16 @@
+class Balls:
+    print('OOp might be confusing at first')
+    def __init__(self,type,sport):
+        self.type =type 
+        self.sport = sport
+    def goals(self):
+        print('i like how the football looks')
+        
+obj = Balls('football','sport')
+print (obj.type)
+obj.goals()    
+        
+        
+        
+        
+        
