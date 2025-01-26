77f508f20cc70341f6e8074b40499ecf657a5c78
Bl4Z3qwert <boubounehtoboh@gmail.com>
2025-01-26 19:10:53 +0000

    fyiood

---
+++ odd.py
@@ -0,0 +1,26 @@
+# num = int(input('enter number'))
+# print(num)
+# if num%2 ==0:
+#     print('this number is even')
+# else:
+#     print('this number is odd')
+
+# print('start pattern')
+# for i in range(1,20):
+#     for j in range(i):
+#         print('*',end="")
+#     print("")    
+
+# print('start pattern')
+# for i in range(1,20):
+#     for j in range(i):
+#         print('$',end="")
+#     print("")    
+    
+
+age = int(input("enter your age"))
+if age >18:
+    print('you can vote')
+else:
+    print('you cant vote')    
+    
