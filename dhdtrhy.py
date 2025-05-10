2b6eeff6d183e417b156bdf9d6d470ccfdeee1d2
Bl4Z3qwert <boubounehtoboh@gmail.com>
2025-05-10 18:56:19 +0000

    arrays

---
+++ dhdtrhy.py
@@ -0,0 +1,8 @@
+import array as arr
+array_num = arr.array('i',[  2,3,1,5,4,7,6,8,4,2,1,3 ])
+print('Original array: ' + str(array_num))
+print('Number of occurances of repited numbers: ' + str(array_num))
+array_num.reverse()
+print('Reverse order of numbers')
+print(str(array_num))
+
