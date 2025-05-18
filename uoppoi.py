1610887be23ab00b38e04bf3276080d19d1980fc
Bl4Z3qwert <boubounehtoboh@gmail.com>
2025-05-18 18:46:43 +0000

    self

---
+++ uoppoi.py
@@ -0,0 +1,14 @@
+class student:
+    grade =6
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+        
+    def details(self):
+        print(f'my name is{self.name} im {self.age} im in grade{self.grade} ')
+obj = student('ken', 13)
+obj2 = student('der', 12) 
+obj.details()
+obj2.details()           
+    
+    
