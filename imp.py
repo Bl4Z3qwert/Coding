a02e3d0c2f8eb4ea03b21c12bfc2f622e109e7e7
Bl4Z3qwert <boubounehtoboh@gmail.com>
2025-06-14 18:59:18 +0000

    pygame intro

--- imp.py
+++ imp.py
@@ -2,7 +2,13 @@
 
 import pygame
 pygame.init()
-screen=pygame.display.set_mode((40,50))
+screen=pygame.display.set_mode((500,500))
+surface = pygame.Surface((300,300))
+screen.blit(image=(300,300))
+pygame.image.load('OIP.jfif')
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("My first game screen")
+GRAY = (58, 58, 58)
 done=False
 while not done:
     for event in pygame.event.get():
