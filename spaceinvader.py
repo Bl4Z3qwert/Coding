72c0babff1587681430f5d3ff2c32bb79d9bbd46
Bl4Z3qwert <boubounehtoboh@gmail.com>
2025-06-28 19:21:03 +0000

    gsgs

--- spaceinvader.py
+++ spaceinvader.py
@@ -36,7 +36,8 @@ def fire(x,y):
     screen.blit(bullet,(x + 16 , y-10))
     
 def is_collision(x1,y1,x2,y2):
-    return math.hypot(x2-x1,y2-y1)<27 
+    return math.hypot(x2-x1,y2-y1)<27
+ 
 
 
 
@@ -50,6 +51,36 @@ while run:
         
        elif event.type==pygame.KEYDOWN:
            if event.key==pygame.K_LEFT:
-               player_move=5    
+               player_move=-5    
            if event.key==pygame.K_RIGHT:
-               player_move=5       
+               player_move=5   
+           if     event.key==pygame.K_SPACE and bullet_state=='reday':
+               bullet_x=player_x
+               bullet_y=player_y
+               fire(bullet_x,bullet_y)
+       elif event.type==pygame.KEYUP:
+           if event.key  in [pygame.K_LEFT,pygame.K_RIGHT] :
+               player_move=0
+   player_x+=player_move
+   player_x=max(0,min(player_x,736))
+   
+   
+   
+   enemy_x+=enemy_move_x
+   if enemy_x<=0 or enemy_x>=736:
+       enemy_move_x*=-1
+       enemy_y+=enemy_move_y
+   if bullet_state=='fire':
+       fire(bullet_x,bullet_y)
+       bullet_y-=bullet_move
+       if bullet_y <=0:
+           bullet_state='ready'
+           
+   if bullet_state=='fire' and is_collision(enemy_x,enemy_move_y,bullet_x,bullet_y):
+       bullet_state='ready'
+       enemy_x=random.randint(0,736)  
+       enemy_y=50
+       
+   screen.blit(player,(player_x,player_y))        
+   screen.blit(enemy,(enemy_x,enemy_y))
+   pygame.display.update()         
